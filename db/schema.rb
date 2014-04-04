09d94d8064255f8c66c2e7cde708013bf27e24f1
App Academy Student <student@appacademy.io>
2014-04-04 16:46:11 -0400

    removes comments table. added it prematurely.

--- db/schema.rb
+++ db/schema.rb
@@ -11,18 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140404194549) do
+ActiveRecord::Schema.define(version: 20140404154657) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "comments", force: true do |t|
-    t.integer  "user_id"
-    t.integer  "goal_id"
-    t.text     "body"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
 
   create_table "goals", force: true do |t|
     t.string   "title"
