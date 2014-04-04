73681610052597d2d4ace725c471f711c2cdbeee
App Academy Student <student@appacademy.io>
2014-04-04 18:35:46 -0400

    had to do a quick modify to reconcile a change

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,10 @@
 class Comment < ActiveRecord::Base
+<<<<<<< Local Changes
 
   validates :body, presence: true
 
   belongs_to :commentable, polymorphic: true
 
+=======
+>>>>>>> External Changes
 end
