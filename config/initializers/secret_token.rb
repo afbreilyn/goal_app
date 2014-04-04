53ac603367a2a3ab4d922fc1253b9c38ba576082
App Academy Student <student@appacademy.io>
2014-04-04 09:24:41 -0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ThingsToDo::Application.config.secret_key_base = '94619ed087673d9646d1ce33e4729ddff06ef0c912984077cbcc273efb81f09452ad8b268fc96c0e72a3cf7ba6678614b0462dc1207ad17089755efd7d0f8d98'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ThingsToDo::Application.config.session_store :cookie_store, key: '_things-to-do_session'
